b2530f1b7aa17db592d3e28913dc1d49739dcbfb
Jeremy Bem <jeremy1@gmail.com>
2010-07-18 22:28:17 -0400

    silly enhancement of Id module

--- typing/id.ml
+++ typing/id.ml
@@ -3,3 +3,20 @@ let create x = x
 let create_persistent x = x
 let name x = x
 let same x y = (x = y)
+
+
+module M = Map.Make(struct
+                      type t = string
+                      let compare = compare
+                    end)
+
+type 'a tbl = 'a M.t
+
+let empty = M.empty
+let add = M.add
+let find_same = M.find
+let find_name = M.find
+let keys m = M.fold (fun k v l -> k :: l) m []
+let find_all p m = M.fold (fun k v l -> if p v then k :: l else l) m []
+let iter f m = M.iter (fun k v -> f v) m
+
