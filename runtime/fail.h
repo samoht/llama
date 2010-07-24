98e596468432ca966f6f6b0220e855e2ed34f901
Jeremy Bem <jeremy1@gmail.com>
2010-07-24 18:26:24 -0400

    Fix wordcount program--in the process discover why we want predefined exceptions...

--- runtime/fail.h
+++ runtime/fail.h
@@ -6,14 +6,14 @@
 #include "misc.h"
 #include "mlvalues.h"
 
-#define OUT_OF_MEMORY_EXN 0     /* "Exc","Out_of_memory",1 */
-#define SYS_ERROR_EXN 1         /* "Sys","Sys_error",1 */
-#define FAILURE_EXN 2           /* "Exc","Failure",3 */
-#define INVALID_EXN 3           /* "Exc","Invalid_argument",2 */
-#define END_OF_FILE_EXN 4       /* "Io","End_of_file",1 */
-#define ZERO_DIVIDE_EXN 5       /* "Int","Division_by_zero",1 */
-#define BREAK_EXN 6             /* "Sys","Break",2 */
-#define NOT_FOUND_EXN 7         /* "Exc","Not_found",4 */
+#define OUT_OF_MEMORY_EXN 0     /* "Pervasives","Out_of_memory",2 */
+#define SYS_ERROR_EXN 1         /* "Pervasives","Sys_error",1 */
+#define FAILURE_EXN 2           /* "Pervasives","Failure",4 */
+#define INVALID_EXN 3           /* "Pervasives","Invalid_argument",3 */
+#define END_OF_FILE_EXN 4       /* "Pervasives","End_of_file",8 */
+#define ZERO_DIVIDE_EXN 5       /* "Pervasives","Division_by_zero",7 */
+#define BREAK_EXN 6             /* "Sys","Break",1 */
+#define NOT_FOUND_EXN 7         /* "Pervasives","Not_found",5 */
 #define UNIX_ERROR_EXN 8        /* "Unix","Unix_error",1 */
 #define GRAPHIC_FAILURE_EXN 9   /* "Graphics","Graphic_failure",1 */
 #define PARSE_FAILURE_EXN 10    /* "Stream","Parse_failure",1 */
