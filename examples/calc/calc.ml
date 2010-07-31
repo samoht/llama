437a81c48d2f5a2a6dff88013f379f7b4a1336b3
Jeremy Bem <jeremy1@gmail.com>
2010-07-31 13:10:57 -0400

    fix bootstrap, fix calc example

--- examples/calc/calc.ml
+++ examples/calc/calc.ml
@@ -1,25 +1,6 @@
 open Parser
 let _ =
-  print_endline "enter * when ready to switch from lexing to parsing";
-  flush stdout;
   let lexbuf = Lexing.from_channel stdin in
-  let done_lexing = ref false in
-  while not !done_lexing do
-    let tok = Lexer.token lexbuf in
-    print_string "got a token: ";
-    print_endline
-      begin match tok with
-        | INT x -> string_of_int x
-        | PLUS -> "+"
-        | MINUS -> "-"
-        | TIMES -> done_lexing := true; "*"
-        | DIV -> "/"
-        | LPAREN -> "("
-        | RPAREN -> ")"
-        | EOL -> "(end-of-line)"
-      end;
-    flush stdout;
-  done;
   try
     while true do
       let result = Parser.main Lexer.token lexbuf in
