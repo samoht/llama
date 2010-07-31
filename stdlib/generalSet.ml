cc5341b74bf18cea3be9dc2a230f833182ade9d7
Jeremy Bem <jeremy1@gmail.com>
2010-07-31 17:36:39 -0400

    improve generalset

--- stdlib/generalSet.ml
+++ stdlib/generalSet.ml
@@ -15,16 +15,20 @@
 
 (* Sets over ordered types *)
 
-    type 'elt t = { desc : 'elt t_desc; cmp : 'elt -> 'elt -> int }
+type 'a cmp = 'a -> 'a -> int
 
-    and 'elt t_desc = Empty | Node of 'elt t * 'elt * 'elt t * int
+type 'a t = Empty of 'a cmp | Node of 'a cmp * 'a t * 'a * 'a t * int
 
     (* Sets are represented by balanced binary trees (the heights of the
        children differ by at most 2 *)
 
-    let height s = match s.desc with
-        Empty -> 0
-      | Node(_, _, _, h) -> h
+    let comparator = function
+        Empty cmp -> cmp
+      | Node(cmp, _, _, _, _) -> cmp
+
+    let height = function
+        Empty _ -> 0
+      | Node(_, _, _, _, h) -> h
 
     (* Creates a new node with left son l, value v and right son r.
        We must have all elements of l < v < all elements of r.
@@ -32,9 +36,9 @@
        Inline expansion of height for better speed. *)
 
     let create l v r =
-      let hl = match l.desc with Empty -> 0 | Node(_,_,_,h) -> h in
-      let hr = match r.desc with Empty -> 0 | Node(_,_,_,h) -> h in
-      { desc = Node(l, v, r, (if hl >= hr then hl + 1 else hr + 1)); cmp = l.cmp }
+      let hl = match l with Empty _ -> 0 | Node(_,_,_,_,h) -> h in
+      let hr = match r with Empty _ -> 0 | Node(_,_,_,_,h) -> h in
+      Node(comparator l, l, v, r, (if hl >= hr then hl + 1 else hr + 1))
 
     (* Same as create, but performs one step of rebalancing if necessary.
        Assumes l and r balanced and | height l - height r | <= 3.
@@ -42,42 +46,41 @@
        where no rebalancing is required. *)
 
     let bal l v r =
-      let hl = match l.desc with Empty -> 0 | Node(_,_,_,h) -> h in
-      let hr = match r.desc with Empty -> 0 | Node(_,_,_,h) -> h in
+      let hl = match l with Empty _ -> 0 | Node(_,_,_,_,h) -> h in
+      let hr = match r with Empty _ -> 0 | Node(_,_,_,_,h) -> h in
       if hl > hr + 2 then begin
-        match l.desc with
-          Empty -> invalid_arg "Set.bal"
-        | Node(ll, lv, lr, _) ->
+        match l with
+          Empty _ -> invalid_arg "Set.bal"
+        | Node(_, ll, lv, lr, _) ->
             if height ll >= height lr then
               create ll lv (create lr v r)
             else begin
-              match lr.desc with
-                Empty -> invalid_arg "Set.bal"
-              | Node(lrl, lrv, lrr, _)->
+              match lr with
+                Empty _ -> invalid_arg "Set.bal"
+              | Node(_, lrl, lrv, lrr, _)->
                   create (create ll lv lrl) lrv (create lrr v r)
             end
       end else if hr > hl + 2 then begin
-        match r.desc with
-          Empty -> invalid_arg "Set.bal"
-        | Node(rl, rv, rr, _) ->
+        match r with
+          Empty _ -> invalid_arg "Set.bal"
+        | Node(_, rl, rv, rr, _) ->
             if height rr >= height rl then
               create (create l v rl) rv rr
             else begin
-              match rl.desc with
-                Empty -> invalid_arg "Set.bal"
-              | Node(rll, rlv, rlr, _) ->
+              match rl with
+                Empty _ -> invalid_arg "Set.bal"
+              | Node(_, rll, rlv, rlr, _) ->
                   create (create l v rll) rlv (create rlr rv rr)
             end
       end else
-        { desc = Node(l, v, r, (if hl >= hr then hl + 1 else hr + 1));
-          cmp = l.cmp }
+        Node(comparator l, l, v, r, (if hl >= hr then hl + 1 else hr + 1))
 
     (* Insertion of one element *)
 
-    let rec add x s = match s.desc with
-        Empty -> { desc = Node(s, x, s, 1); cmp = s.cmp }
-      | Node(l, v, r, _) as t ->
-          let c = s.cmp x v in
+    let rec add x s = match s with
+        Empty cmp -> Node(cmp, s, x, s, 1)
+      | Node(cmp, l, v, r, _) ->
+          let c = cmp x v in
           if c = 0 then s else
           if c < 0 then bal (add x l) v r else bal l v (add x r)
 
@@ -85,41 +88,41 @@
        relative heights of l and r. *)
 
     let rec join l v r =
-      match (l.desc, r.desc) with
-        (Empty, _) -> add v r
-      | (_, Empty) -> add v l
-      | (Node(ll, lv, lr, lh), Node(rl, rv, rr, rh)) ->
+      match (l, r) with
+        (Empty _, _) -> add v r
+      | (_, Empty _) -> add v l
+      | (Node(_, ll, lv, lr, lh), Node(_, rl, rv, rr, rh)) ->
           if lh > rh + 2 then bal ll lv (join lr v r) else
           if rh > lh + 2 then bal (join l v rl) rv rr else
           create l v r
 
     (* Smallest and greatest element of a set *)
 
-    let rec min_elt s = match s.desc with
-        Empty -> raise Not_found
-      | Node({desc=Empty}, v, r, _) -> v
-      | Node(l, v, r, _) -> min_elt l
+    let rec min_elt = function
+        Empty _ -> raise Not_found
+      | Node(_, Empty _, v, r, _) -> v
+      | Node(_, l, v, r, _) -> min_elt l
 
-    let rec max_elt s = match s.desc with
-        Empty -> raise Not_found
-      | Node(l, v, {desc=Empty}, _) -> v
-      | Node(l, v, r, _) -> max_elt r
+    let rec max_elt = function
+        Empty _ -> raise Not_found
+      | Node(_, l, v, Empty _, _) -> v
+      | Node(_, l, v, r, _) -> max_elt r
 
     (* Remove the smallest element of the given set *)
 
-    let rec remove_min_elt s = match s.desc with
-        Empty -> invalid_arg "Set.remove_min_elt"
-      | Node({desc=Empty}, v, r, _) -> r
-      | Node(l, v, r, _) -> bal (remove_min_elt l) v r
+    let rec remove_min_elt = function
+        Empty _ -> invalid_arg "Set.remove_min_elt"
+      | Node(_, Empty _, v, r, _) -> r
+      | Node(_, l, v, r, _) -> bal (remove_min_elt l) v r
 
     (* Merge two trees l and r into one.
        All elements of l must precede the elements of r.
        Assume | height l - height r | <= 2. *)
 
     let merge t1 t2 =
-      match (t1.desc, t2.desc) with
-        (Empty, _) -> t2
-      | (_, Empty) -> t1
+      match (t1, t2) with
+        (Empty _, _) -> t2
+      | (_, Empty _) -> t1
       | (_, _) -> bal t1 (min_elt t2) (remove_min_elt t2)
 
     (* Merge two trees l and r into one.
@@ -127,9 +130,9 @@
        No assumption on the heights of l and r. *)
 
     let concat t1 t2 =
-      match (t1.desc, t2.desc) with
-        (Empty, _) -> t2
-      | (_, Empty) -> t1
+      match (t1, t2) with
+        (Empty _, _) -> t2
+      | (_, Empty _) -> t1
       | (_, _) -> join t1 (min_elt t2) (remove_min_elt t2)
 
     (* Splitting.  split x s returns a triple (l, present, r) where
@@ -138,11 +141,11 @@
         - present is false if s contains no element equal to x,
           or true if s contains an element equal to x. *)
 
-    let rec split x s = match s.desc with
-        Empty ->
+    let rec split x s = match s with
+        Empty _ ->
           (s, false, s)
-      | Node(l, v, r, _) ->
-          let c = s.cmp x v in
+      | Node(cmp, l, v, r, _) ->
+          let c = cmp x v in
           if c = 0 then (l, true, r)
           else if c < 0 then
             let (ll, pres, rl) = split x l in (ll, pres, join rl v r)
@@ -151,28 +154,28 @@
 
     (* Implementation of the set operations *)
 
-    let empty cmp = { desc = Empty; cmp = cmp }
+    let empty cmp = Empty cmp
 
-    let is_empty s = match s.desc with Empty -> true | _ -> false
+    let is_empty = function Empty _ -> true | _ -> false
 
-    let rec mem x s = match s.desc with
-        Empty -> false
-      | Node(l, v, r, _) ->
-          let c = s.cmp x v in
+    let rec mem x = function
+        Empty _ -> false
+      | Node(cmp, l, v, r, _) ->
+          let c = cmp x v in
           c = 0 || mem x (if c < 0 then l else r)
 
-    let rec remove x s = match s.desc with
-        Empty -> s
-      | Node(l, v, r, _) ->
-          let c = s.cmp x v in
+    let rec remove x = function
+        Empty _ as s -> s
+      | Node(cmp, l, v, r, _) ->
+          let c = cmp x v in
           if c = 0 then merge l r else
           if c < 0 then bal (remove x l) v r else bal l v (remove x r)
 
     let rec union s1 s2 =
-      match (s1.desc, s2.desc) with
-        (Empty, _) -> s2
-      | (_, Empty) -> s1
-      | (Node(l1, v1, r1, h1), Node(l2, v2, r2, h2)) ->
+      match (s1, s2) with
+        (Empty _, _) -> s2
+      | (_, Empty _) -> s1
+      | (Node(_, l1, v1, r1, h1), Node(_, l2, v2, r2, h2)) ->
           if h1 >= h2 then
             if h2 = 1 then add v2 s1 else begin
               let (l2, _, r2) = split v1 s2 in
@@ -185,10 +188,10 @@
             end
 
     let rec inter s1 s2 =
-      match (s1.desc, s2.desc) with
-        (Empty, _) -> s1
-      | (_, Empty) -> s2
-      | (Node(l1, v1, r1, _), _) ->
+      match (s1, s2) with
+        (Empty _, _) -> s1
+      | (_, Empty _) -> s2
+      | (Node(_, l1, v1, r1, _), _) ->
           match split v1 s2 with
             (l2, false, r2) ->
               concat (inter l1 l2) (inter r1 r2)
@@ -196,10 +199,10 @@
               join (inter l1 l2) v1 (inter r1 r2)
 
     let rec diff s1 s2 =
-      match (s1.desc, s2.desc) with
-        (Empty, _) -> s1
-      | (_, Empty) -> s1
-      | (Node(l1, v1, r1, _), _) ->
+      match (s1, s2) with
+        (Empty _, _) -> s1
+      | (_, Empty _) -> s1
+      | (Node(_, l1, v1, r1, _), _) ->
           match split v1 s2 with
             (l2, false, r2) ->
               join (diff l1 l2) v1 (diff r1 r2)
@@ -209,9 +212,9 @@
     type 'elt enumeration = End | More of 'elt * 'elt t * 'elt enumeration
 
     let rec cons_enum s e =
-      match s.desc with
-        Empty -> e
-      | Node(l, v, r, _) -> cons_enum l (More(v, r, e))
+      match s with
+        Empty _ -> e
+      | Node(_, l, v, r, _) -> cons_enum l (More(v, r, e))
 
     let rec compare_aux cmp e1 e2 =
         match (e1, e2) with
@@ -225,67 +228,68 @@
           else compare_aux cmp (cons_enum r1 e1) (cons_enum r2 e2)
 
     let compare s1 s2 =
-      if s1.cmp != s2.cmp then invalid_arg "GeneralSet.compare";
-      compare_aux s1.cmp (cons_enum s1 End) (cons_enum s2 End)
+      let cmp1 = comparator s1 in
+      let cmp2 = comparator s2 in
+      if cmp1 != cmp2 then invalid_arg "Set.compare";
+      compare_aux cmp1 (cons_enum s1 End) (cons_enum s2 End)
 
     let equal s1 s2 =
       compare s1 s2 = 0
 
-    let rec subset s1 s2 = subset_aux s1.desc s2
-
-    and subset_aux t1 s2 =
-      match (t1, s2.desc) with
-        Empty, _ ->
+    let rec subset s1 s2 =
+      match (s1, s2) with
+        Empty _, _ ->
           true
-      | _, Empty ->
+      | _, Empty _ ->
           false
-      | Node (l1, v1, r1, _), Node (l2, v2, r2, _) ->
-          let c = s2.cmp v1 v2 in
+      | Node (cmp, l1, v1, r1, _), Node (_, l2, v2, r2, _) ->
+          let c = cmp v1 v2 in
           if c = 0 then
             subset l1 l2 && subset r1 r2
           else if c < 0 then
-            subset_aux (Node (l1, v1, empty s2.cmp, 0)) l2 && subset r1 s2
+            subset (Node (cmp, l1, v1, empty cmp, 0)) l2 && subset r1 s2
           else
-            subset_aux (Node (empty s2.cmp, v1, r1, 0)) r2 && subset l1 s2
+            subset (Node (cmp, empty cmp, v1, r1, 0)) r2 && subset l1 s2
 
-    let rec iter f s = match s.desc with
-        Empty -> ()
-      | Node(l, v, r, _) -> iter f l; f v; iter f r
+    let rec iter f = function
+        Empty _ -> ()
+      | Node(_, l, v, r, _) -> iter f l; f v; iter f r
 
     let rec fold f s accu =
-      match s.desc with
-        Empty -> accu
-      | Node(l, v, r, _) -> fold f r (f v (fold f l accu))
+      match s with
+        Empty _ -> accu
+      | Node(_, l, v, r, _) -> fold f r (f v (fold f l accu))
 
-    let rec for_all p s = match s.desc with
-        Empty -> true
-      | Node(l, v, r, _) -> p v && for_all p l && for_all p r
+    let rec for_all p = function
+        Empty _ -> true
+      | Node(_, l, v, r, _) -> p v && for_all p l && for_all p r
 
-    let rec exists p s = match s.desc with
-        Empty -> false
-      | Node(l, v, r, _) -> p v || exists p l || exists p r
+    let rec exists p = function
+        Empty _ -> false
+      | Node(_, l, v, r, _) -> p v || exists p l || exists p r
 
     let filter p s =
-      let rec filt accu s' = match s'.desc with
-        | Empty -> accu
-        | Node(l, v, r, _) ->
+      let rec filt accu = function
+        | Empty _ -> accu
+        | Node(_, l, v, r, _) ->
             filt (filt (if p v then add v accu else accu) l) r in
-      filt (empty s.cmp) s
+      filt (empty (comparator s)) s
 
     let partition p s =
-      let rec part (t, f as accu) s' = match s'.desc with
-        | Empty -> accu
-        | Node(l, v, r, _) ->
+      let rec part (t, f as accu) = function
+        | Empty _ -> accu
+        | Node(_, l, v, r, _) ->
             part (part (if p v then (add v t, f) else (t, add v f)) l) r in
-      part (empty s.cmp, empty s.cmp) s
+      let e = empty (comparator s) in
+      part (e, e) s
 
-    let rec cardinal s = match s.desc with
-        Empty -> 0
-      | Node(l, v, r, _) -> cardinal l + 1 + cardinal r
+    let rec cardinal = function
+        Empty _ -> 0
+      | Node(_, l, v, r, _) -> cardinal l + 1 + cardinal r
 
-    let rec elements_aux accu s = match s.desc with
-        Empty -> accu
-      | Node(l, v, r, _) -> elements_aux (v :: elements_aux accu r) l
+    let rec elements_aux accu = function
+        Empty _ -> accu
+      | Node(_, l, v, r, _) -> elements_aux (v :: elements_aux accu r) l
 
     let elements s =
       elements_aux [] s
