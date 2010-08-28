5f6cd866533a90cea3e5964dd07b924d90767552
Jeremy Bem <jeremy1@gmail.com>
2010-08-27 21:21:24 -0400

    add more comments to Base

--- typing/base.ml
+++ typing/base.ml
@@ -1,65 +1,70 @@
+(* Where Llama Light really starts. *)
+
 (* ---------------------------------------------------------------------- *)
 (* Utility types.                                                         *)
 (* ---------------------------------------------------------------------- *)
 
 type module_id =
-    Module_builtin
-  | Module of string
-  | Module_toplevel
+    Module_builtin                     (* Predefined types and exceptions *)
+  | Module of string                   (* Named module *)
+  | Module_toplevel                    (* Toplevel definitions *)
 
 type constructor_tag =
-    Tag_constant of int
-  | Tag_block of int
-  | Tag_exception
+    Tag_constant of int                (* Constant constructor (an int) *)
+  | Tag_block of int                   (* Regular constructor (a block) *)
+  | Tag_exception                      (* Exception constructor *)
 
 type value_kind =
-    Val_reg
-  | Val_prim of Primitive.description
+    Val_reg                            (* Regular value *)
+  | Val_prim of Primitive.description  (* Primitive *)
 
 type rec_status =
-    Rec_first
-  | Rec_next
+    Rec_first                          (* first in a recursive group *)
+  | Rec_next                           (* not first in a recursive group *)
 
 (* ---------------------------------------------------------------------- *)
-(* Types representing the essential global entities.                      *)
+(* Types representing the essential named entities.                       *)
 (* ---------------------------------------------------------------------- *)
 
-(* these get compared with (==) *)
+(* These get compared with (==). *)
 (* They are generic so they can be used in-memory or on disk. *)
 
-(* TODO: pull in descriptive comments from ocaml *)
-
 type 'ty gen_type_constructor =
-  { tcs_module : module_id;
-    tcs_name : string;
-    tcs_params : 'ty list;
+  { tcs_module : module_id;     (* Defining module *)
+    tcs_name : string;          (* Name of the type constructor *)
+    tcs_params : 'ty list;      (* List of type parameters *)
     mutable tcs_kind : 'ty gen_type_constructor_kind }
+                                (* Kind of the type constructor *)
 
 and 'ty gen_type_constructor_kind =
-    Tcs_abstract
-  | Tcs_variant of 'ty gen_constructor list
-  | Tcs_record of 'ty gen_label list
-  | Tcs_abbrev of 'ty
+    Tcs_abstract                             (* Abstract type *)
+  | Tcs_variant of 'ty gen_constructor list  (* Sum type *)
+  | Tcs_record of 'ty gen_label list         (* Record type *)
+  | Tcs_abbrev of 'ty                        (* Abbreviation type *)
 
 and 'ty gen_constructor =
   { mutable cs_tcs : 'ty gen_type_constructor;
-    cs_module : module_id;
-    cs_name : string;
-    cs_args : 'ty list;
-    cs_tag : constructor_tag }
+                                (* Parent type constructor *)
+    cs_module : module_id;      (* Defining module *)
+    cs_name : string;           (* Name of the constructor *)
+    cs_args : 'ty list;         (* Type of the arguments *)
+    cs_tag : constructor_tag }  (* Tag for heap blocks *)
 
 and 'ty gen_label =
   { lbl_tcs : 'ty gen_type_constructor;
-    lbl_name : string;
-    lbl_arg : 'ty;
-    lbl_mut : bool;
-    lbl_pos : int }
+                                (* Parent type constructor *)
+    lbl_name : string;          (* Name of the label *)
+    lbl_arg : 'ty;              (* Type of the argument *)
+    lbl_mut : bool;             (* Is this a mutable field? *)
+    lbl_pos : int }             (* Position in block *)
 
 type 'ty gen_value =
-  { val_module : module_id;
-    val_name : string;
-    val_type : 'ty;
-    val_kind : value_kind }
+  { val_module : module_id;     (* Defining module *)
+    val_name : string;          (* Name of the value *)
+    val_type : 'ty;             (* Type of the value *)
+    val_kind : value_kind }     (* Is this a primitive? *)
+
+(* Internal representation of a signature. *)
 
 type 'ty gen_signature_item =
     Sig_value of 'ty gen_value
@@ -78,7 +83,8 @@ type llama_type =
   | Ttuple of llama_type list
   | Tconstr of type_constructor * llama_type list
 
-and type_parameter = { param_name : string }  (* compared with (==) *)
+and type_parameter = { param_name : string }
+  (* Parameters are compared with (==). *)
 
 and type_constructor = llama_type gen_type_constructor
 
@@ -98,16 +104,20 @@ type signature = llama_type gen_signature
 (* Utilities.                                                             *)
 (* ---------------------------------------------------------------------- *)
 
+let tcs_arity tcs = List.length tcs.tcs_params   (* Number of type arguments *)
+let tcs_res tcs = Tconstr (tcs, tcs.tcs_params)  (* Type w/ default arguments *)
+let cs_arity cs = List.length cs.cs_args         (* Number of arguments *)
+let cs_res cs = tcs_res cs.cs_tcs                (* Type of the result *)
+let lbl_module lbl = lbl.lbl_tcs.tcs_module      (* Defining module *)
+  (* The analogous definition doesn't work for constructors because of
+     exceptions. *)
+let lbl_res lbl = tcs_res lbl.lbl_tcs            (* Type of the result *)
+
+(* TODO: just use longidents everywhere. *)
 type qualified_id = module_id * string
 let tcs_qualid tcs = (tcs.tcs_module, tcs.tcs_name)
-let tcs_arity tcs = List.length tcs.tcs_params
-let tcs_res tcs = Tconstr (tcs, tcs.tcs_params)
 let cs_qualid cs = (cs.cs_module, cs.cs_name)
-let cs_arity cs = List.length cs.cs_args
-let cs_res cs = tcs_res cs.cs_tcs
-let lbl_module lbl = lbl.lbl_tcs.tcs_module
 let lbl_qualid lbl = (lbl_module lbl, lbl.lbl_name)
-let lbl_res lbl = tcs_res lbl.lbl_tcs
 let val_qualid v = (v.val_module, v.val_name)
 
 let int_to_alpha i =
@@ -123,7 +133,6 @@ let get_constructors tcs =
   match tcs.tcs_kind with
       Tcs_variant cs_list -> cs_list
     | _ -> failwith "Base.get_constructors"
-
 let get_labels tcs =
   match tcs.tcs_kind with
       Tcs_record lbl_list -> lbl_list
