927fe664021213067497b01417f70365d5a84c5d
samoht <thomas@gazagnaire.com>
2011-02-25 12:04:51 +0100

    Add missing file ...

---
+++ frontlib/log.ml
@@ -0,0 +1,31 @@
+(* Some debug functions *)
+
+(* Name of the environment variable *)
+let llamadebug = "LLAMADEBUG"
+let sep        = ':'
+
+(* XXX: put that in the standard library *)
+let split str sep =
+  let rec split_rec pos =
+    if pos >= String.length str then [] else begin
+      try
+        let newpos = String.index_from str pos sep in
+        String.sub str pos (newpos - pos) :: split_rec (newpos + 1)
+      with Not_found ->
+        [String.sub str pos (String.length str - pos)]
+    end in
+  split_rec 0
+
+(* The predicate read some environment variables to filter the messags to print out *)
+let predicates section =
+  try
+    let s = Sys.getenv llamadebug in
+    List.mem section (split s sep)
+  with _ ->
+    false
+
+let debug section fmt =
+  let fn message =
+    if predicates section then
+      Printf.eprintf "%-10s | %s\n%!" section message in
+  Printf.kprintf fn fmt
