b87639256a9f3e2479d9188437dba3d710a1a427
nahoj <johan.grande@inria.fr>
2011-04-28 19:02:45 +0200

    Remove useless files.

--- stdlib/test2.ml
+++
@@ -1,8 +0,0 @@
-(* ../llamac-new test2.ml && ocamlrun a.out *)
-
-let _ =
-  let s = "14" in
-  let l = [ 1; 4; 7; int_of_string s ] in
-  let l = List.rev l in
-  List.iter (fun n -> print_endline (string_of_int n)) l
-
